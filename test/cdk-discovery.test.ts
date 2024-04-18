4257b726fbbeae169143fd57196c421bd188253d
drumont <drumont06@gmail.com>
2024-04-18 22:10:29 +0200

    Initial commit

---
+++ test/cdk-discovery.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkDiscovery from '../lib/cdk-discovery-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-discovery-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkDiscovery.CdkDiscoveryStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
